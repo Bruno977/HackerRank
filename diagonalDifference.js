25c28c71e662f78428c008944e682e693e5a6acd
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-03-08 20:45:30 -0300

    Finish diagonalDIfference

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,10 +1,14 @@
-function diagonalDifference(arr) {
-    let leftToRight = 0
-    let rigthToLeft = 0
-    for (let lin = 0; lin < arr.length; lin++){
-      for (let col = 0; col < arr.length; col++) {
-        console.log(arr[col][lin])
-        lin++
-      }
-    }
-}diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ])
+let matriz = (arr) => {
+    let rigthToLeft = arr.length
+    let leftToRigth = 0
+    let sumLeft = 0
+    let sumRigth = 0
+    arr.forEach(element => {
+        rigthToLeft--
+        sumRigth += element[rigthToLeft]
+        sumLeft += (element[leftToRigth])
+        leftToRigth++
+    });
+    return (Math.abs(sumLeft - sumRigth))
+}
+console.log(matriz([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
