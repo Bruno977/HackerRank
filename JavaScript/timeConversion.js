92e8c0954fa808aaa9af120c3dbc2925d314aa06
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-03-14 09:24:31 -0300

    Review This challenge

--- JavaScript/timeConversion.js
+++ JavaScript/timeConversion.js
@@ -1,12 +1,19 @@
 let timeConversion = (hour) => {
     let array = []
+    let count = 0
     hour.split("").forEach(element => {
         array.push(element)
-        if (element === "P" || element === "A" || element === "M") {
-            array.pop()
+        if(element === "P"){
+          count++
+          array.pop()
         }
+        if (element === "A" || element === "M") {array.pop()}
     });
-    let newArray = array.join("").split(":")
-    console.log(newArray)
+    let newArray = array.join(""). split(":")
+    if(count === 1){
+      let calcPm = Number(newArray[0]) + 12
+      newArray.splice(0, 1, `${calcPm}`)
+    }
+  console.log(newArray)
 }
-timeConversion("07:05:45PM")
+timeConversion("11:01:00PM")
