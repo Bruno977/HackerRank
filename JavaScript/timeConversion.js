42f0bedc03554086d85fd2eaad17e222a0c3a30f
Bruno Rodrigues <bruno_151997@hotmail.com>
2021-03-14 11:22:20 -0300

    Review this challenge

--- JavaScript/timeConversion.js
+++ JavaScript/timeConversion.js
@@ -1,24 +1,8 @@
 let timeConversion = (hour) => {
-    let array = []
-    let count = 0
-    hour.split("").forEach(element => {
-        array.push(element)
-        if(element === "P"){
-          count++
-          array.pop()
-        }
-        if (element === "A" || element === "M") {array.pop()}
-    });
-<<<<<<< HEAD
-    let newArray = array.join("").split(":")
-    console.log(Number(newArray[0]))
-=======
-    let newArray = array.join(""). split(":")
-    if(count === 1){
-      let calcPm = Number(newArray[0]) + 12
-      newArray.splice(0, 1, `${calcPm}`)
-    }
-  console.log(newArray)
->>>>>>> 92e8c0954fa808aaa9af120c3dbc2925d314aa06
+  let newHour = hour.split(":")
+  if (hour.indexOf('PM') > -1) {
+    let calcPm = Number(newHour[0]) + 12
+    String(calcPm.splice(0, 1, `${calcPm}`))
+  }
 }
 timeConversion("11:01:00PM")
