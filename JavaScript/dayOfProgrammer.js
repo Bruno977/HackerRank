f017abf1f71b24a0ff8e3f0a8ebc5401f7eac4bf
Bruno Rodrigues <47644815+Bruno977@users.noreply.github.com>
2021-03-22 09:57:18 -0300

    Finish challenge

--- JavaScript/dayOfProgrammer.js
+++ JavaScript/dayOfProgrammer.js
@@ -1,9 +1,18 @@
 function dayOfProgrammer(year) {
- if((year % 4 === 0 && year % 100 != 0) || year % 400 ===0){
-    return (`12.09.${year}`)
- }else if(year % 4 === 0 ){
-    return(`12.09.${year}`)   
- }else{
-    return(`13.09.${year}`)
- }
-}dayOfProgrammer( 2100 )
+  if (year < 1918){
+    if (year % 4 === 0){
+      return (`12.09.${year}`)
+    }else{
+      return(`13.09.${year}`)
+    }
+  }else if (year > 1918){
+     if((year % 4 === 0 && year % 100 != 0) || year % 400 ===0){
+      return (`12.09.${year}`)
+     }else{
+       return(`13.09.${year}`)
+     }
+  }
+  else{
+    return(`26.09.${year}`)
+  }
+}dayOfProgrammer( 1918 )
